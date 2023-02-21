acdd21ed37653ee9fafd9a51fa0deed56a113858
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-02-21 23:45:20 +0530

    tanstake queary add and get data

--- src/Components/Page/Cart/ProductS.js
+++ src/Components/Page/Cart/ProductS.js
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
 import ProductsCart from './ProductsCart';
 
 const ProductS = () => {
-    const [products , setProducts] = useState([])
-    useEffect(()=>{
-        fetch('http://localhost:5000/products')
-        .then(res => res.json())
-        .then(data => setProducts(data))
-    } ,[])
+   
 
-    console.log(products);
+    const { data :products = [] , isLoading } = useQuery({
+        queryKey: ["products"],
+        queryFn: async () => {
+          const res = await fetch(
+            `http://localhost:5000/products`
+          );
+          const data = await res.json();
+          return data;
+        }})
+        if(isLoading){
+            return <div className="w-16 mx-auto mt-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-blue-400"></div>
+        }
     
     return (
         <div>

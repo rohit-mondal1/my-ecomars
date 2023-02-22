7a6a0eec622ca070209e4dba3abc5b3c017f56fd
rahul-sarker18 <rahulsarker1495@gmail.com>
2023-02-23 00:38:57 +0530

    cart make

--- src/Components/Page/Cart/ProductsCart.js
+++ src/Components/Page/Cart/ProductsCart.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { SiSaucelabs } from "react-icons/si";
+import { BsFillCartCheckFill } from "react-icons/bs";
 
 const ProductsCart = ({ product }) => {
   const {
@@ -21,24 +23,38 @@ const ProductsCart = ({ product }) => {
     <div>
       <div className="card card-compact my-4 mx-4 w-80 bg-base-100 shadow-xl">
         <figure>
-          <img
-            src={img}
-            alt="Shoes"
-          />
+          <img src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{name?.length < 22 ? name : name.slice(0 , 22) + '..' }</h2>
+          <h2 className="text-xl">
+            {name?.length < 22 ? name : name.slice(0, 22) + ".."}
+          </h2>
 
-          <div className="text-xl font-medium">
+          <div className=" font-medium">
             <h1>price : {price} $</h1>
           </div>
-          <div className="flex justify-between  text-xl font-medium">
-            <h1> {ratings} </h1>
-            <h1> {price} </h1>
+          <div className="flex justify-start items-center    font-medium">
+            <div className="bg-emerald-700 flex justify-start items-center  px-2">
+              <h1 className="text-white"> {ratings} </h1>
+              <div className="rating rating-sm">
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-white"
+                  checked
+                />
+              </div>
+            </div>
+            ({ratingsCount})
           </div>
-         
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+
+          <div className="card-actions justify-between text-white">
+            <button className="btn btn-secondary gap-2">
+              Buy Now <SiSaucelabs />
+            </button>
+            <button className="btn btn-accent gap-2">
+              Add to cart<BsFillCartCheckFill />
+            </button>
           </div>
         </div>
       </div>
